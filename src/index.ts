5ca32cc2436a8c2ad5e14aa002011af560517547
Anthony Naddeo <anthony.naddeo@gmail.com>
2018-01-06 17:37:51 -0800

    Bumped for publish.
    Adding the re-export back in so you can just depend on this directly. I
    didn't want to do it before because I was thinking of having
    applications that share update/model between platforms but its better to
    be able to depend on one package half the time than none of the time.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as ReactDom from 'react-dom';
 import * as React from 'react';
 import { Switchable, Program, Model, RootView } from 'hathaway-core';
 
+export * from 'hathaway-core';
+
 export function start<M extends Model, Msg extends Switchable>(program: Program<M, Msg>) {
     ReactDom.render(React.createElement(RootView, { program }), program.renderTarget);
 }
