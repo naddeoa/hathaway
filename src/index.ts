aee4153fe449ef44caf920abe9cf3e7186cfb27e
Anthony Naddeo <anthony.naddeo@gmail.com>
2018-02-04 11:31:24 -0800

    Add render DOM to the start function
    Doesn't belong in the shared code

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { Switchable, Program, Model, RootView } from 'hathaway-core';
 
 export * from 'hathaway-core';
 
-export function start<M extends Model, Msg extends Switchable>(program: Program<M, Msg>) {
-    ReactDom.render(React.createElement(RootView, { program }), program.renderTarget);
+export function start<M extends Model, Msg extends Switchable>(renderTarget: HTMLElement, program: Program<M, Msg>) {
+    ReactDom.render(React.createElement(RootView, { program }), renderTarget);
 }
